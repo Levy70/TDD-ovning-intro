bf00ffdd4238773659fb4435a0d51b7b082b5d24
kamphol23 <44361002+kamphol23@users.noreply.github.com>
2023-11-13 15:16:16 +0100

    datarow

--- FizzBuzz Test/KalkylatorTests.cs
+++ FizzBuzz Test/KalkylatorTests.cs
@@ -2,21 +2,24 @@ using FizzBuzz;
 
 namespace FizzBuzz_Test
 {
+    [DataRow(3, "Fizz")]
+    [DataRow(5, "Buzz")]
+    [DataRow(15, "FizzBuzz")]
+
     [TestClass]
     public class KalkylatorTests
     {
         [TestMethod]
 
-        public void När_FizzBuzzKalkyl_EjImplementerad_Then_ShouldThrow_NotImplementedException()
+        public void När_FizzBuzzKalkyl_EjImplementerad_Then_ShouldThrow_NotImplementedException(int number, string expectedResult)
         {
             // Arrange
-            int randomNummer = 15;
 
             // Act
-            string result = Kalkylator.FizzBuzzKalkyl(randomNummer);
+            string result = Kalkylator.FizzBuzzKalkyl(number);
 
             //Assert
-            Assert.AreEqual("FizzBuzz", result);
+            Assert.AreEqual(expectedResult, result);
 
         }
     }
